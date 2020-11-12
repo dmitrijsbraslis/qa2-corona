10bb9e7ae600f4dce9ef1877e451910f6c4b05cf
Crazy <dmitrijs.braslis@gmail.com>
2020-11-12 19:41:29 +0200

    Initial commit

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+    private String name = "Gospodin";
+
+    @Test
+    public void firstTest() {
+        System.out.println("Hello, corona!");
+
+        int sum = sumTwoDigits(15, 45);
+        sumTwoDigits(sum, 15);
+        System.out.println("And the sum is: " + sum + " and bla bla " + sum);
+
+        System.out.println(sumTwoDigits(9, 13));
+        System.out.println(name);
+    }
+
+    private int sumTwoDigits(int a, int b) {
+        System.out.println(name);
+        int c = a + b;
+        return c;
+    }
+}
